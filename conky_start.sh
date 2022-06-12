33f5b2be7b300bf57556227cd5f28b56026319de
RAprogramm <andrey.rozanov.vl@gmail.com>
2022-06-12 13:12:00 +0900

    weather and datetime

--- conky_start.sh
+++ conky_start.sh
@@ -4,6 +4,7 @@ left=~/.config/conky/sides/conky_left.conf
 right=~/.config/conky/sides/right_side.conf
 clock_calendar=~/.config/conky/clock_calendar/clock_calendar.conf
 weather=~/.config/conky/weather/weather.conf
+player=~/.config/conky/player/player.conf
 
 cd $(dirname $0)
 killall conky
@@ -14,6 +15,7 @@ then
     conky --daemonize --quiet --config=$right
     conky --daemonize --quiet --config=$clock_calendar
     conky --daemonize --quiet --config=$weather
+    conky --daemonize --quiet --config=$player
     echo "conky was started successfull!"
 else 
     echo "conky was not started..."
--- weather/weather.lua
+++ weather/weather.lua
@@ -14,55 +14,73 @@ Pressure_units = (Cf == "metric" and " hPa" or "mph")
 Currenttime = os.date("!%Y%m%d%H%M%S")
 
 File_exists = function(name)
-	F = io.open(name, "r")
-	if F ~= nil then
-		io.close(F)
-		return true
-	else
-		return false
-	end
+    F = io.open(name, "r")
+    if F ~= nil then
+        io.close(F)
+        return true
+    else
+        return false
+    end
 end
 
 if File_exists("weather.json") then
-	Cache = io.open("weather.json", "r")
-	Data = Json.decode(Cache:read())
-	Cache.close(Cache)
-	Timepassed = os.difftime(Currenttime, Data.timestamp)
+    Cache = io.open("weather.json", "r")
+    Data = Json.decode(Cache:read())
+    Cache.close(Cache)
+    Timepassed = os.difftime(Currenttime, Data.timestamp)
 else
-	Timepassed = 6000
+    Timepassed = 6000
 end
 
 Makecache = function(s)
-	Cache = io.open("weather.json", "w+")
-	s.timestamp = Currenttime
-	Save = Json.encode(s)
-	Cache:write(Save)
-	Cache.close(Cache)
+    Cache = io.open("weather.json", "w+")
+    s.timestamp = Currenttime
+    Save = Json.encode(s)
+    Cache:write(Save)
+    Cache.close(Cache)
 end
 
 if Timepassed < 3600 then
-	Response = Data
+    Response = Data
 else
-	Weather = Http.request(("%sid=%s&units=%s&APPID=%s"):format(Api_url, Cityid, Cf, Apikey))
-	if Weather then
-		Response = Json.decode(Weather)
-		Makecache(Response)
-	else
-		Response = Data
-	end
+    Weather = Http.request(("%sid=%s&units=%s&APPID=%s"):format(Api_url, Cityid, Cf, Apikey))
+    if Weather then
+        Response = Json.decode(Weather)
+        Makecache(Response)
+    else
+        Response = Data
+    end
 end
 
 math.round = function(n)
-	return math.floor(n + 0.5)
+    return math.floor(n + 0.5)
 end
 
 Degrees_to_direction = function(d)
-	Val = math.round(d / 22.5)
-	Directions = { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" }
-	return Directions[Val % 16]
+    Val = math.round(d / 22.5)
+    Directions = {
+        "↑",
+        "NNE",
+        "↗",
+        "ENE",
+        "→",
+        "ESE",
+        "↘",
+        "SSE",
+        "↓",
+        "SSW",
+        "↙",
+        "WSW",
+        "←",
+        "WNW",
+        "↖",
+        "NNW",
+    }
+    return Directions[Val % 16]
 end
 
 Temp = Response.main.temp
+-- Conditions = "light intensity shower rain"
 Conditions = Response.weather[1].description
 Icon = Response.weather[1].id
 Pressure = Response.main.pressure
@@ -73,36 +91,35 @@ Sunrise = os.date("%H:%M", Response.sys.sunrise)
 Sunset = os.date("%H:%M", Response.sys.sunset)
 
 Conky_text = [[
-${font2}${color2}│
-${font2}${color2}│
-${font2}${color2}│
-${font2}${color2}│
-${font2}${color2}│
-${font2}${color2}│${image ~/.config/conky/weather/icons/%s.png -p 20,57 -s 80x80}${color1}${font :size=25} ${voffset -40}${offset 100}%s%s${color}
-  ${voffset -16}$font2$color2────────────────$color0
-${voffset -2}${font2}${color2}│$font$color0         ${font3}%s
-${font2}${color2}│
-${font2}${color2}│ ${font4}${color0}Pressure: $font${color1}%s %s${color}
-${font2}${color2}│ ${font4}${color0}Humidity: $font${color1}%s%%${color}
-${font2}${color2}│ ${font4}${color0}Wind: $font${color1}%s%s %s${color}
-${font2}${color2}│
-${font2}${color2}│    ${font}${color}sunrise           sunset
-${font2}${color2}│${image ~/.config/conky/weather/icons/sunrise.png -p 65,263 -s 32x32}    ${color1}%s${color}    ${image ~/.config/conky/weather/icons/sunset.png -p 165,263 -s 32x32}${color1}%s${color}
+
+
+
+
+
+${image ~/.config/conky/weather/icons/%s.png -p 20,57 -s 80x80}${color1}${font Bitstream Vera Sans Mono Nerd Font Complete Mono:bold:size=25} ${voffset -40}${offset 100}%s%s${color}
+${voffset -11}$font2$color2─────────────────$color0
+$font$color0   ${font5}%s
+${voffset -1}$font2$color2───────────────────────$color0
+${font3}${color0}Pressure: ${font Bitstream Vera Sans Mono Nerd Font Complete Mono:bold:size=13}%s%s${color}
+${font3}${color0}Humidity: ${font Bitstream Vera Sans Mono Nerd Font Complete Mono:bold:size=13}%s%%${color}
+${font3}${color0}Wind: ${font Bitstream Vera Sans Mono Nerd Font Complete Mono:bold:size=14}%s%s ${font2}%s${color}
+${font3}Sunrise: ${font Bitstream Vera Sans Mono Nerd Font Complete Mono:bold:size=14}%s
+${font3}Sunset: ${font Bitstream Vera Sans Mono Nerd Font Complete Mono:bold:size=14}%s
 ]]
 
 io.write(
-	(Conky_text):format(
-		Icon,
-		math.round(Temp),
-		Measure,
-		Conditions,
-		Pressure,
-		Pressure_units,
-		Humidity,
-		math.round(Wind),
-		Wind_units,
-		Deg,
-		Sunrise,
-		Sunset
-	)
+    (Conky_text):format(
+        Icon,
+        math.round(Temp),
+        Measure,
+        Conditions,
+        Pressure,
+        Pressure_units,
+        Humidity,
+        math.round(Wind),
+        Wind_units,
+        Deg,
+        Sunrise,
+        Sunset
+    )
 )
